593b4662e0baa0ebe93033d08232048c94386bb5
Nelli Butenko <butenkonelly93@gmail.com>
2021-04-08 03:12:06 +0000

    Edge cases

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,17 @@
 const request = require('request');
 const fs = require('fs');
+const isValid = require('is-valid-path');
 const url = process.argv[2];
 const path = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
+request(url, (err, response, body) => {
+  fs.access(path, fs.F_OK, (err) => {
+    if (isValid(path)) {
+      throw new Error('This path is not valid')
+    }
+  });
+  if (err) {
+    throw new Error('Something went wrong with URL')
   } else {
     fs.writeFile(path, body, () => {
     console.log('Downloaded and saved 1235 bytes to ./index.html.')
